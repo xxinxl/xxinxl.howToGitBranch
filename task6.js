71f7ea9a9d5a2bb1dd05a67216c87904be2ab0e0
Natalia <natalia.lyamina1234@gmail.com>
2023-10-13 15:17:07 +0300

    TEst

--- task6.js
+++
@@ -1,5 +0,0 @@
-const fuelConsumption = 40; //Расход топлива
-const distance = 120; //Дистанция
-const priceFuel = 980; //Стоимость
-
-console.log("Стоимость поездки: " + (distance / fuelConsumption) * priceFuel + " Руб.");
