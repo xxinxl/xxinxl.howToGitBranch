2ee05c9d84e82447c63b001d97d73cb11595c0c4
Natalia <natalia.lyamina1234@gmail.com>
2023-10-13 15:26:20 +0300

    AAAA

--- src/task7.js
+++
@@ -1,7 +0,0 @@
-const a = "5";
-const b = "4";
-const c = "12";
-
-let p = 1/2 * (a + b + c);
-let s = Math.sqrt(p * (p-a) * (p-b) * (p-c));
-console.log(s);
--- src/task8.js
+++
@@ -1,3 +0,0 @@
-const celc = 18;
-let temperature = (celc * 9 / 5) + 32;
-console.log(temperature + ' F ' + celc + 'C');
